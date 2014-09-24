5e0ad4524b90ae3dd25bee2606b88a2eb9c7ba19
John Peterson <jwpeterson@gmail.com>
2014-09-24 10:34:13 -0600

    Print the Jacobi points and weights in a way consistent with what is in libmesh.

--- drivers/jacobi_rule.C
+++ drivers/jacobi_rule.C
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <cstdio>
 #include <vector>
+#include <iomanip>
+#include <sstream>
 
 // The GNU multi-precision library
 #include "gmp.h"
@@ -23,7 +25,11 @@
 // #include "jacobi_constants.h"
 #include "jacobi.h"
 
-
+// Returns a string which contains the 32 decimal digits of x in the
+// form: -9.4489927222288222340758013830322e-01L.  This should really
+// be moved to its own header file, since both this file and the
+// Gauss-Lobatto codes use it.
+std::string fix_string(mpfr_class x);
 
 int main()
 {
@@ -70,7 +76,19 @@ int main()
       jacobi_rule.scale_points(zero, one);
 
       // Print the result
-      jacobi_rule.printxw();
+
+      // The points and weights arrays are 1-based
+      const std::vector<mpfr_class>& x = jacobi_rule.get_points();
+      const std::vector<mpfr_class>& w = jacobi_rule.get_weights();
+
+      for (unsigned i=1; i<x.size(); ++i)
+        std::cout << "_points[" << std::setw(2) << i-1 << "](0) = " << fix_string(x[i]) << ";\n";
+
+      // blank line
+      std::cout << std::endl;
+
+      for (unsigned i=1; i<w.size(); ++i)
+        std::cout << "_weights[" << std::setw(2) << i-1 << "]   = " << fix_string(w[i]) << ";\n";
 
       std::cout << "\n";
     }
@@ -81,4 +99,53 @@ int main()
 
 
   return 0;
+}
+
+
+
+std::string fix_string(mpfr_class x)
+{
+  // Go to some extra trouble to print e-01L instead of e-1
+  std::ostringstream number_stream;
+  number_stream.precision(32);
+  number_stream.setf(std::ios_base::scientific);
+  number_stream << x;
+  std::string number = number_stream.str();
+
+  // We almost always want to append an L (long double literal) but not always
+  bool append_L = true;
+
+  // Find the exponent 'e'.
+  size_t e_pos = number.find('e');
+
+  // If 'e' is not found...
+  if (e_pos == std::string::npos)
+    {
+      if (number == "0")
+        {
+          // if number == "0", append a decimal point
+          number.append(".");
+          append_L = false;
+        }
+      else
+        {
+          // otherwise, append "e+00"
+          number.append("e+00");
+        }
+    }
+  else
+    {
+      // If 'e' is found, insert a leading zero to be consistent
+      // with the Gauss rules in libmesh.  The number of digits
+      // used in the exponent is implementation-dependent.  To be
+      // totally general, we should check to see how many zeros
+      // there are, and only insert exactly as many as we need...
+      number.insert(e_pos+2, "0");
+    }
+
+  // The L suffix in C/C++ makes the compiler treat a number literal as 'long double'
+  if (append_L)
+    number.append("L");
+
+  return number;
 }
