fb7ef4b9f458ae8bf8e1579ff809be115c39a3f6
John Peterson <jw.peterson@inl.gov>
2014-09-28 14:16:02 -0600

    Moving fix_string() function to a central location.

--- include/common_definitions.h
+++ include/common_definitions.h
@@ -1,6 +1,16 @@
 #ifndef __common_definitions__
 #define __common_definitions__
 
+#include <string>
+
+// For mpfr_class.  It may be possible to forward declare this, but
+// I'm not sure how.
+#include "gmpfrxx.h"
+
 typedef double Real;
+
+// Returns a string which contains the 32 decimal digits of x in the form:
+// -9.4489927222288222340758013830322e-01L
+std::string fix_string(mpfr_class& x);
 
 #endif
---
+++ src/common_definitions.C
@@ -0,0 +1,50 @@
+#include <sstream>
+
+#include "common_definitions.h"
+
+std::string fix_string(mpfr_class& x)
+{
+  // Go to some extra trouble to print e-01L instead of e-1
+  std::ostringstream number_stream;
+  number_stream.precision(32);
+  number_stream.setf(std::ios_base::scientific);
+  number_stream << x;
+  std::string number = number_stream.str();
+
+  // We almost always want to append an L (long double literal) but not always
+  bool append_L = true;
+
+  // Find the exponent 'e'.
+  size_t e_pos = number.find('e');
+
+  // If 'e' is not found...
+  if (e_pos == std::string::npos)
+    {
+      if (number == "0")
+        {
+          // if number == "0", append a decimal point
+          number.append(".");
+          append_L = false;
+        }
+      else
+        {
+          // otherwise, append "e+00"
+          number.append("e+00");
+        }
+    }
+  else
+    {
+      // If 'e' is found, insert a leading zero to be consistent
+      // with the Gauss rules in libmesh.  The number of digits
+      // used in the exponent is implementation-dependent.  To be
+      // totally general, we should check to see how many zeros
+      // there are, and only insert exactly as many as we need...
+      number.insert(e_pos+2, "0");
+    }
+
+  // The L suffix in C/C++ makes the compiler treat a number literal as 'long double'
+  if (append_L)
+    number.append("L");
+
+  return number;
+}
