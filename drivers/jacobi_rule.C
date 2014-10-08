01203c45da72dfd19b069eb5c4c9cee8ebe24e45
John Peterson <jwpeterson@gmail.com>
2014-10-08 12:27:06 -0600

    Simplify use of MP constants.
    Remove commented out code.

--- drivers/jacobi_rule.C
+++ drivers/jacobi_rule.C
@@ -8,7 +8,6 @@
 
 // The GNU multi-precision library
 #include "gmp.h"
-// #include "gmpxx.h"
 
 // The MPFR library defines special functions
 // like sin, cos, exp, etc.
@@ -22,7 +21,6 @@
 
 // Header files for this project
 #include "common_definitions.h"
-// #include "jacobi_constants.h"
 #include "jacobi.h"
 
 int main()
@@ -51,23 +49,16 @@ int main()
 
       // Scale Jacobi weights so they sum to 1/3 (alpha==2) or 1/2 (alpha==1)
       if (alpha==2.0)
-        {
-          mpfr_class one_third(1.0);
-          one_third /= 3.0;
-          jacobi_rule.scale_weights(one_third);
-        }
+        jacobi_rule.scale_weights(mpfr_class(1.0)/mpfr_class(3.0));
+
       else if (alpha==1.0)
-        {
-          jacobi_rule.scale_weights(0.5);
-        }
+        jacobi_rule.scale_weights(0.5);
+
       else
-        {
-          std::cout << "Warning: weights unscaled!" << std::endl;
-        }
+        std::cout << "Warning: weights unscaled!" << std::endl;
 
       // Scale Jacobi points so they lie on [0, 1]
-      mpfr_class zero(0.0), one(1.0);
-      jacobi_rule.scale_points(zero, one);
+      jacobi_rule.scale_points(mpfr_class(0.0), mpfr_class(1.0));
 
       // Print the result
 
