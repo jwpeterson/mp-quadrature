d4045333c5d078ea5dc93e6a442badd8a13b80c4
John Peterson <jw.peterson@inl.gov>
2014-10-30 08:24:34 -0600

    Add support for sorting the points and weights
    Initially added std::partition() code to keep the negative and
    positive weights together, however indirect-sorting the weights
    (rather than just partitioning) probably makes more sense...

--- drivers/gm_rule.C
+++ drivers/gm_rule.C
@@ -34,6 +34,7 @@ std::string format_rational(const mpq_class & number)
 }
 
 
+
 int main(int argc, char** argv)
 {
   std::cout.precision(32);
@@ -55,6 +56,13 @@ int main(int argc, char** argv)
   const std::vector<Point<mpq_class> >& x = gm.get_points();
   const std::vector<mpq_class>& w = gm.get_weights();
 
+  // Use the custom indirect_partition() or indirect_sort() functions
+  // define an ordering of the points and weights without actually
+  // moving data around in those vectors.
+  std::vector<size_t> indices;
+  //indirect_partition(w.begin(), w.end(), indices); // partitions weights on either side of zero
+  indirect_sort(w.begin(), w.end(), indices); // sorts the weights
+
   std::cout << "Index " << s
             << " rule has degree " << 2*s+1
             << ", and " << x.size() << " point(s)." << std::endl;
@@ -65,19 +73,51 @@ int main(int argc, char** argv)
   std::cout << "_points.resize(" << x.size() << ");" << std::endl;
   std::cout << "_weights.resize(" << x.size() << ");\n" << std::endl;
 
+  // Determine how many spaces to use to make everything line up in the printout
+  unsigned print_width = (x.size() > 10) ? 2 : 1;
+
+  // Pre-compute the widest entry of each column.  We don't have to sort to do this...
+  size_t
+    widest_x = 0,
+    widest_y = 0,
+    widest_z = 0,
+    widest_w = 0;
+  for (unsigned i=0; i<x.size(); ++i)
+    {
+      widest_x = std::max(widest_x, format_rational(x[i](0)).size());
+      widest_y = std::max(widest_y, format_rational(x[i](1)).size());
+      widest_z = std::max(widest_z, format_rational(x[i](2)).size());
+      widest_w = std::max(widest_w, format_rational(w[i]).size());
+    }
+
+  // std::cout << "widest_x = " << widest_x << " "
+  //           << "widest_y = " << widest_y << " "
+  //           << "widest_z = " << widest_z << " "
+  //           << "widest_w = " << widest_w << " "
+  //           << std::endl;
+
+  // If false, print the points and weights in the order they are
+  // computed, if true print them in the weight-sorted order.
+  // Eventually make this selectable on the command line.
+  const bool print_sorted = true;
+
+  // If false, will not compute all the verification integrals.  This
+  // can be nice for high-order rules which take an extremely long
+  // time to verify.  Eventually make this selectable on the command
+  // line.
+  const bool do_verification = true;
+
   // Print the points and weights
   for (unsigned i=0; i<x.size(); ++i)
     {
-      // std::cout << x[i](0) << ", "
-      //           << x[i](1) << ", "
-      //           << x[i](2) << ", "
-      //           << w[i] << std::endl;
+      // Choose the sorted or "natural" ordering
+      unsigned mapped_index = print_sorted ? indices[i] : i;
 
       // Print points in C++ code that can be used in libmesh
-      std::cout << "_points[" << std::setw(2) << i << "](0) = " << format_rational(x[i](0)) << ";\t"
-                << "_points[" << std::setw(2) << i << "](1) = " << format_rational(x[i](1)) << ";\t"
-                << "_points[" << std::setw(2) << i << "](2) = " << format_rational(x[i](2)) << ";\t"
-                << "_weights[" << std::setw(2) << i << "] = "   << format_rational(w[i]) << ";"
+      std::cout << "_points[" << std::setw(print_width) << i << "](0) = " << std::setw(widest_x) << format_rational(x[mapped_index](0)) << ";  "
+                << "_points[" << std::setw(print_width) << i << "](1) = " << std::setw(widest_y) << format_rational(x[mapped_index](1)) << ";  "
+                << "_points[" << std::setw(print_width) << i << "](2) = " << std::setw(widest_z) << format_rational(x[mapped_index](2)) << ";  "
+                << "_weights[" << std::setw(print_width) << i << "] = "   << std::setw(widest_w) << format_rational(w[mapped_index]) << ";"
                 << std::endl;
 
       sum_weights += w[i];
@@ -87,78 +127,90 @@ int main(int argc, char** argv)
   mpq_class error_weights = abs(sum_weights - mpq_class("1/6"));
   std::cout << "Error in sum of weights = " << error_weights << std::endl;
 
-  // Mostly copied from conical_product_3D.C, the verification code
-  // for tets should probably be factored out into a common
-  // function...
-  std::cout << "\nVerifying rule..." << std::endl;
-  unsigned max_order = 2*s+1;
-  for (unsigned x_power=0; x_power<max_order; ++x_power)
-    for (unsigned y_power=0; y_power<max_order; ++y_power)
-      for (unsigned z_power=0; z_power<max_order; ++z_power)
-        {
-          // Only try to integrate polynomials we can integrate exactly
-          if (x_power + y_power + z_power > max_order)
-            continue;
-
-          // conical_w, conical_x, and conical_y use 0-based array access!
-          mpq_class sum = 0;
-          for (unsigned n_qp=0; n_qp<x.size(); ++n_qp)
-            sum += w[n_qp] * pow(x[n_qp](0), x_power) * pow(x[n_qp](1), y_power) * pow(x[n_qp](2), z_power);
-
-          // std::cout << "quadrature = " << sum << std::endl;
-
-          // Compute the exact solution as described above, divide
-          // term-by-term to avoid huge numbers/overflows (even though
-          // this is GMP).
-          mpq_class analytical = 1.0;
-          {
-            // Sort the a, b, c values
-            unsigned sorted_powers[3] = {x_power, y_power, z_power};
-            std::sort(sorted_powers, sorted_powers+3);
-
-            // Cancel the largest power with the denominator, fill in the
-            // entries for the remaining numerator terms and the denominator.
-            std::vector<unsigned>
-              numerator_1(sorted_powers[0] > 1 ? sorted_powers[0]-1 : 0),
-              numerator_2(sorted_powers[1] > 1 ? sorted_powers[1]-1 : 0),
-              denominator(3 + sorted_powers[0] + sorted_powers[1]);
-
-            // Fill up the vectors with sequences starting at the right values.
-            iota(numerator_1.begin(), numerator_1.end(), 2);
-            iota(numerator_2.begin(), numerator_2.end(), 2);
-            iota(denominator.begin(), denominator.end(), sorted_powers[2]+1);
-
-            // The denominator is guaranteed to have the most terms...
-            for (unsigned i=0; i<denominator.size(); ++i)
-              {
-                if (i < numerator_1.size())
-                  analytical *= numerator_1[i];
-
-                if (i < numerator_2.size())
-                  analytical *= numerator_2[i];
-
-                analytical /= denominator[i];
-              }
-          }
-
-          // std::cout << "analytical = " << analytical << std::endl;
-
-          // Compute the absolute error:
-          mpq_class abs_err = abs(sum-analytical);
-
-          // Print message.  In 3D, this is just way too much output.
-          //std::cout << "Computing integral of: x^" << x_power << " y^" << y_power << " z^" << z_power
-          //          << ", abs_err = " << abs_err << std::endl;
-
-          // Abort if error is too large.
-          if (abs_err > mpq_class(1.e-30))
+  if (do_verification)
+    {
+      // Mostly copied from conical_product_3D.C, the verification code
+      // for tets should probably be factored out into a common
+      // function...
+      std::cout << "\nVerifying rule..." << std::endl;
+      unsigned max_order = 2*s+1;
+      for (unsigned x_power=0; x_power<max_order; ++x_power)
+        for (unsigned y_power=0; y_power<max_order; ++y_power)
+          for (unsigned z_power=0; z_power<max_order; ++z_power)
             {
-              std::cerr << "Quadrature error too large, possible problem with points and weights!" << std::endl;
-              std::abort();
-            }
-        }
+              // Only try to integrate polynomials we can integrate exactly
+              if (x_power + y_power + z_power > max_order)
+                continue;
 
-  std::cout << "... Verification complete!" << std::endl;
+              // Compute the quadrature result
+              mpq_class sum = 0;
+              for (unsigned n_qp=0; n_qp<x.size(); ++n_qp)
+                {
+                  // Choose the sorted or "natural" ordering
+                  unsigned mapped_index = print_sorted ? indices[n_qp] : n_qp;
+
+                  sum +=
+                    w[mapped_index] *
+                    pow(x[mapped_index](0), x_power) *
+                    pow(x[mapped_index](1), y_power) *
+                    pow(x[mapped_index](2), z_power);
+                }
+
+              // std::cout << "quadrature = " << sum << std::endl;
+
+              // Compute the exact solution as described above, divide
+              // term-by-term to avoid huge numbers/overflows (even though
+              // this is GMP).
+              mpq_class analytical = 1.0;
+              {
+                // Sort the a, b, c values
+                unsigned sorted_powers[3] = {x_power, y_power, z_power};
+                std::sort(sorted_powers, sorted_powers+3);
+
+                // Cancel the largest power with the denominator, fill in the
+                // entries for the remaining numerator terms and the denominator.
+                std::vector<unsigned>
+                  numerator_1(sorted_powers[0] > 1 ? sorted_powers[0]-1 : 0),
+                  numerator_2(sorted_powers[1] > 1 ? sorted_powers[1]-1 : 0),
+                  denominator(3 + sorted_powers[0] + sorted_powers[1]);
+
+                // Fill up the vectors with sequences starting at the right values.
+                iota(numerator_1.begin(), numerator_1.end(), 2);
+                iota(numerator_2.begin(), numerator_2.end(), 2);
+                iota(denominator.begin(), denominator.end(), sorted_powers[2]+1);
+
+                // The denominator is guaranteed to have the most terms...
+                for (unsigned i=0; i<denominator.size(); ++i)
+                  {
+                    if (i < numerator_1.size())
+                      analytical *= numerator_1[i];
+
+                    if (i < numerator_2.size())
+                      analytical *= numerator_2[i];
+
+                    analytical /= denominator[i];
+                  }
+              }
+
+              // std::cout << "analytical = " << analytical << std::endl;
+
+              // Compute the absolute error:
+              mpq_class abs_err = abs(sum-analytical);
+
+              // Print message.  In 3D, this is just way too much output.
+              //std::cout << "Computing integral of: x^" << x_power << " y^" << y_power << " z^" << z_power
+              //          << ", abs_err = " << abs_err << std::endl;
+
+              // Abort if error is too large.
+              if (abs_err > mpq_class(1.e-30))
+                {
+                  std::cerr << "Quadrature error too large, possible problem with points and weights!" << std::endl;
+                  std::abort();
+                }
+            }
+
+      std::cout << "... Verification complete!" << std::endl;
+    } // end if (do_verification)
 
   return 0;
 }
--- include/common_definitions.h
+++ include/common_definitions.h
@@ -2,6 +2,8 @@
 #define __common_definitions__
 
 #include <string>
+#include <vector>
+#include <algorithm> // std::partition
 
 // For mpfr_class.  It may be possible to forward declare this, but
 // I'm not sure how.
@@ -24,6 +26,8 @@ void iota (ForwardIter first, ForwardIter last, T value)
       ++first;
     }
 }
+
+
 
 /**
  * A simple class representing an arbitrary-precision point in
@@ -57,4 +61,99 @@ private:
   T _coords[3];
 };
 
-#endif
+
+
+/**
+ * An indirect unary comparison object to be used with the
+ * indirect_partition function.  An obvious generalization would be to
+ * also template on a custom unary comparison object.
+ */
+template <class RandomAccessIterator>
+struct indirect_unary_is_negative
+{
+  // ctor
+  indirect_unary_is_negative(RandomAccessIterator r) :
+    _iterator(r)
+  {}
+
+  // unary comparison operator
+  bool operator()(size_t lhs)
+  {
+    return _iterator[lhs] < 0;
+  }
+
+private:
+  RandomAccessIterator _iterator;
+};
+
+
+
+/**
+ * A binary less-than comparison object.  An obvious generalization
+ * would be to also template on a custom comparison object while doing
+ * the indirect comparison.
+ */
+template <class RandomAccessIterator>
+struct indirect_less_than
+{
+  // ctor
+  indirect_less_than(RandomAccessIterator r) :
+    _iterator(r)
+  {}
+
+  // comparison operator - calls the user's comparison function on
+  // v[lhs] and v[rhs]
+  bool operator()(size_t lhs, size_t rhs)
+    {
+      // Note: operator[] is defined for random access iterators!
+      return _iterator[lhs] < _iterator[rhs];
+    }
+
+private:
+  RandomAccessIterator _iterator;
+};
+
+
+
+/**
+ * Calls std::partition() using an indirect comparison operator so
+ * that we can partition multiple vectors at the same time.
+ */
+template <class RandomAccessIterator>
+void indirect_partition(RandomAccessIterator beg,
+                        RandomAccessIterator end,
+                        std::vector<size_t>& indices)
+{
+  // Initialize the indices array
+  indices.resize(std::distance(beg, end));
+  iota(indices.begin(), indices.end(), 0);
+
+  // Construct indirect comparison object
+  indirect_unary_is_negative<RandomAccessIterator> comp(beg);
+
+  // Call std::partition on the indices with the indirect comparison object
+  std::partition(indices.begin(), indices.end(), comp);
+}
+
+
+
+/**
+ * Similar to std::partition but calls std::sort indirectly.
+ */
+template <class RandomAccessIterator>
+void indirect_sort(RandomAccessIterator beg,
+                   RandomAccessIterator end,
+                   std::vector<size_t>& indices)
+{
+  // Initialize the indices array
+  indices.resize(std::distance(beg, end));
+  iota(indices.begin(), indices.end(), 0);
+
+  // Construct comparator object
+  indirect_less_than<RandomAccessIterator> comp(beg);
+
+  // Sort the indices, based on the data
+  std::sort(indices.begin(), indices.end(), comp);
+}
+
+#endif // __common_definitions__
