80b7f7a1f5457b6bd4c3756d8058869b8c1128a6
John Peterson <jwpeterson@gmail.com>
2014-11-12 19:28:02 -0700

    Use exact integral function in conical_product_2D.C

--- drivers/conical_product_2D.C
+++ drivers/conical_product_2D.C
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include "gauss.h"
 #include "jacobi.h"
+#include "exact.h"
 
 // In this program we compute multi-precision
 // points and weights to be used in Conical Product
@@ -128,33 +129,8 @@ int main(int argc, char** argv)
 
         // std::cout << "quadrature = " << sum << std::endl;
 
-        // Compute the exact solution as described above, divide
-        // term-by-term to avoid huge numbers/overflows (even though
-        // this is GMP).
-        mpfr_class analytical = 1.0;
-        {
-          unsigned
-            larger_power = std::max(x_power, y_power),
-            smaller_power = std::min(x_power, y_power);
-
-          // Cancel the larger of the two numerator terms with the
-          // denominator, and fill in the remaining entries.
-          std::vector<unsigned>
-            numerator(smaller_power > 1 ? smaller_power-1 : 0),
-            denominator(2+smaller_power);
-
-          // Fill up the vectors with sequences starting at the right values.
-          iota(numerator.begin(), numerator.end(), 2);
-          iota(denominator.begin(), denominator.end(), larger_power+1);
-
-          // The denominator is guaranteed to have more terms...
-          for (unsigned i=0; i<denominator.size(); ++i)
-            {
-              if (i < numerator.size())
-                analytical *= numerator[i];
-              analytical /= denominator[i];
-            }
-        }
+        // Compute the analytical integral value.
+        mpfr_class analytical = exact_tri(x_power, y_power);
 
         // std::cout << "analytical = " << analytical << std::endl;
 
