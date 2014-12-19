394009e7b4e4d0768d5039086fd7d46d3b59405c
John Peterson <jwpeterson@gmail.com>
2014-12-19 15:29:13 -0700

    Moving code from header to C file.

--- include/integrand.h
+++ include/integrand.h
@@ -11,12 +11,10 @@
 struct Integrand
 {
   // Constructor
-  Integrand(const mpfr_class & x0_in) :
-    x0(x0_in)
-  {}
+  Integrand(const mpfr_class & x0_in);
 
   // Destructor
-  virtual ~Integrand() {}
+  virtual ~Integrand();
 
   // The parameter
   mpfr_class x0;
@@ -34,10 +32,7 @@ struct Integrand
   virtual mpfr_class H1_semi_norm_squared() const = 0;
 
   // Returns the H1-norm.
-  mpfr_class H1_norm() const
-  {
-    return sqrt(this->L2_norm_squared() + this->H1_semi_norm_squared());
-  }
+  mpfr_class H1_norm() const;
 };
 
 
@@ -47,42 +42,21 @@ struct Integrand
 struct AlphaSingularity : Integrand
 {
   AlphaSingularity(const mpfr_class & x0_in,
-                   const mpfr_class & alpha_in) :
-    Integrand(x0_in),
-    alpha(alpha_in)
-  {
-    if (alpha <= mpfr_class(1.)/2.)
-      {
-        std::cerr << "alpha must be greater than 1/2." << std::endl;
-        std::abort();
-      }
-  }
+                   const mpfr_class & alpha_in);
 
   mpfr_class alpha;
 
   // f = abs(x-x_0)^{\alpha}, \alpha > \frac{1}{2}
-  virtual mpfr_class f(const mpfr_class & x) const
-  {
-    return pow(abs(x - x0), alpha);
-  }
+  virtual mpfr_class f(const mpfr_class & x) const;
 
   // I(f) = 1/(\alpha+1) [ (1+x_0)^{\alpha+1} + (1-x_0)^{\alpha+1} ]
-  virtual mpfr_class exact() const
-  {
-    return mpfr_class(1.)/(alpha+1.) * (pow(1+x0, alpha+1) + pow(1-x0, alpha+1));
-  }
+  virtual mpfr_class exact() const;
 
   // Compute the L2-norm, squared
-  virtual mpfr_class L2_norm_squared() const
-  {
-    return mpfr_class(1.)/(2.*alpha+1.) * (pow(1+x0, 2.*alpha+1) + pow(1-x0, 2.*alpha+1));
-  }
+  virtual mpfr_class L2_norm_squared() const;
 
   // Compute the H1-semi norm, squared
-  virtual mpfr_class H1_semi_norm_squared() const
-  {
-    return alpha*alpha/(2.*alpha - 1.) * (pow(1+x0, 2.*alpha-1) + pow(1-x0, 2.*alpha-1));
-  }
+  virtual mpfr_class H1_semi_norm_squared() const;
 };
 
 
@@ -92,47 +66,19 @@ struct AlphaSingularity : Integrand
 // { 1 - (x-x0)/(1-x0)
 struct Piecewise : Integrand
 {
-  Piecewise(const mpfr_class & x0_in) :
-    Integrand(x0_in)
-  {}
+  Piecewise(const mpfr_class & x0_in);
 
   // Return the function value
-  virtual mpfr_class f(const mpfr_class & x) const
-  {
-    // Handle edge cases without dividing by zero.
-    if (x0 == -1.)
-      return 0.5*(1.-x);
-    if (x0 == 1.)
-      return 0.5*(1.+x);
-
-    // Handle in-between cases
-    if (x <= x0)
-      return mpfr_class(1.) + (x-x0)/(1.+x0);
-    else
-      return mpfr_class(1.) - (x-x0)/(1.-x0);
-  }
+  virtual mpfr_class f(const mpfr_class & x) const;
 
   // I(f) = 1
-  virtual mpfr_class exact() const
-  {
-    return 1.;
-  }
+  virtual mpfr_class exact() const;
 
   // Compute the L2-norm, squared
-  virtual mpfr_class L2_norm_squared() const
-  {
-    return mpfr_class(2.)/3.;
-  }
+  virtual mpfr_class L2_norm_squared() const;
 
   // Compute the H1-semi norm, squared
-  virtual mpfr_class H1_semi_norm_squared() const
-  {
-    // Handle edge cases without dividing by zero.
-    if (x0 == -1. || x0 == 1.)
-      return 0.5;
-
-    return mpfr_class(1.)/(1. + x0) + mpfr_class(1.)/(1. - x0);
-  }
+  virtual mpfr_class H1_semi_norm_squared() const;
 };
 
 
@@ -143,36 +89,21 @@ struct Piecewise : Integrand
 struct Exponential : Integrand
 {
   Exponential(const mpfr_class & x0_in,
-              const mpfr_class & alpha_in) :
-    Integrand(x0_in),
-    alpha(alpha_in)
-  {}
+              const mpfr_class & alpha_in);
 
   mpfr_class alpha;
 
   // Return the function value
-  virtual mpfr_class f(const mpfr_class & x) const
-  {
-    return exp(-alpha*abs(x-x0));
-  }
+  virtual mpfr_class f(const mpfr_class & x) const;
 
   // True integral
-  virtual mpfr_class exact() const
-  {
-    return 2./alpha * (1. - exp(-alpha)*cosh(alpha*x0));
-  }
+  virtual mpfr_class exact() const;
 
   // Compute the L2-norm, squared
-  virtual mpfr_class L2_norm_squared() const
-  {
-    return 1./alpha * (1. - exp(-2.*alpha)*cosh(2.*alpha*x0));
-  }
+  virtual mpfr_class L2_norm_squared() const;
 
   // Compute the H1-semi norm, squared
-  virtual mpfr_class H1_semi_norm_squared() const
-  {
-    return alpha * (1. - exp(-2.*alpha)*cosh(2.*alpha*x0));
-  }
+  virtual mpfr_class H1_semi_norm_squared() const;
 };
 
 
@@ -182,36 +113,21 @@ struct Exponential : Integrand
 struct Sinusoidal : Integrand
 {
   Sinusoidal(const mpfr_class & x0_in,
-             const mpfr_class & alpha_in) :
-    Integrand(x0_in),
-    alpha(alpha_in)
-  {}
+             const mpfr_class & alpha_in);
 
   mpfr_class alpha;
 
   // Return the function value
-  virtual mpfr_class f(const mpfr_class & x) const
-  {
-    return sin(alpha*(x-x0));
-  }
+  virtual mpfr_class f(const mpfr_class & x) const;
 
   // True integral
-  virtual mpfr_class exact() const
-  {
-    return -2./alpha * sin(alpha) * sin(alpha * x0);
-  }
+  virtual mpfr_class exact() const;
 
   // Compute the L2-norm, squared
-  virtual mpfr_class L2_norm_squared() const
-  {
-    return 1. - sin(2*alpha) * cos(2*alpha*x0) / (2.*alpha);
-  }
+  virtual mpfr_class L2_norm_squared() const;
 
   // Compute the H1-semi norm, squared
-  virtual mpfr_class H1_semi_norm_squared() const
-  {
-    return alpha*alpha * (1. + sin(2*alpha) * cos(2*alpha*x0) / (2.*alpha));
-  }
+  virtual mpfr_class H1_semi_norm_squared() const;
 };
 
 #endif
