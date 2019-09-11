3dd2364a05a6694b46009c8cbe44a52ce3ae2e3b
John Peterson <jwpeterson@gmail.com>
2019-09-10 23:53:49 -0500

    Add a script that we can use to install software on a new instance.

---
+++ make_instance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Commands I ran to install all support software and build mp-quadrature.
+# This assumes you have already cloned the mp-quadrature package into
+# ~/projects/mp-quadrature and are sitting in that directory.
+sudo apt-get update
+sudo apt-get -y install libmpfr-dev
+sudo apt-get -y install g++
+sudo apt-get -y install libnlopt-dev
+sudo apt-get -y install libnlopt0
+sudo apt-get -y install make
+
+# mkdir projects
+# cd projects
+# git clone https://github.com/jwpeterson/mp-quadrature.git
+# cd mp-quadrature
+./configure --with-nlopt-include=/usr/include --with-nlopt-lib=/usr/lib/x86_64-linux-gnu
+make
