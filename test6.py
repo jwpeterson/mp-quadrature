0afd1152c8ae765d1f4cbdb1fb3924d89c3da6b8
John Peterson <jwpeterson@gmail.com>
2019-10-14 16:49:33 -0500

    The lhs and rhs are in fact not equal, so something is wrong with the approach.

--- test6.py
+++ test6.py
@@ -11,14 +11,20 @@ median orbits and 6 QPs. We initially analyzed this rule numerically
 in "test.py", but here the goal is to analyze it using sympy.
 """
 
-def charpoly(xvec, *args):
+def mypoly(xvec, *args):
     sigma = args[0]
     x = xvec[0]
     # print ('x={}, type(x)={}'.format(x, type(x)))
     # print ('sigma={}, type(sigma)={}'.format(sigma, type(sigma)))
     r1 = (9 + math.sqrt(21)) / 30
     r2 = (9 - math.sqrt(21)) / 30
-    resid = x * (x-r1) * (x-r2) - sigma
+
+    # Factored form
+    # resid = x * (x-r1) * (x-r2) - sigma
+
+    # Unfactored form
+    resid = 15*x**2 - 9*x + 1 - sigma
+
     # print ('r1={}, type(r1)={}'.format(r1, type(r1)))
     # print ('r2={}'.format(r2))
     # print ('resid={}, type(resid)={}'.format(resid, type(resid)))
@@ -104,48 +110,65 @@ print('eta3 = {}'.format(eta3))
 # eta1 = 1/40 + 3*eta3
 # eta1 = 1/360 + 2*eta3
 # in agreement with our original analysis of this problem.
+print('---')
 print('eta1 - (1./40 + 3 * eta3)={} (should be zero)'.format(eta1 - (Fraction(1,40) + 3 * eta3)))
 print('eta2 - (1./360 + 2 * eta3)={} (should be zero)'.format(eta2 - (Fraction(1,360) + 2 * eta3)))
 
-print('---')
 # We should be able to derive other solutions by choosing two values
 # somewhat arbitrarily:
-# 1.) xi = w2/w1
+# 1.) Set xi = w2/w1
 # 2.) x2_new = r2 + dx
-# where r2 = x2_soln is the second root above, by first computing
-# the right-hand side:
+# where r2 = x2_soln is the second root above, and dx is a small
+# distance, by first computing the right-hand side:
 # sigma := -xi * (r2 + dx) * (r2 - r1 + dx) * dx.
 # and then finding the root x of
 # x * (x-r1) * (x-r2) - sigma = 0
-# which is closest to r2
-xi = 4*math.sqrt(21)/25 + 31./25 # ~1.973
-dx = .02
+# which is closest to r1
+# Note: For the baseline solution, xi = 4*math.sqrt(21)/25 + 31./25 ~ 1.973
+xi = 2.4
+dx = .01
+x1_old = float(x1_soln.evalf())
 x2_new = float(x2_soln.evalf() + dx)
-sigma = float((-xi * x2_new * ((x2_soln - x1_soln).evalf() + dx) * dx))
+# sigma = float((-xi * x2_new * (x2_new - x1_soln.evalf()) * dx))
+sigma = -xi * x2_new * (15*x2_new**2 - 9*x2_new + 1)
 
+print('---')
 print('xi={}'.format(xi))
 print('sigma={}'.format(sigma))
 print('dx={}'.format(dx))
+print('x2_new={}'.format(x2_new))
+print('x1_old={}'.format(x1_old))
 
-(x1_new, infodict, iflag, mesg) = fsolve(charpoly,
-                                      float(x2_soln.evalf()),
-                                      args=sigma,
-                                      full_output=True)
+(sol, infodict, iflag, mesg) = fsolve(mypoly,
+                                         x1_old,
+                                         args=sigma,
+                                         full_output=True)
 
 # Once xi is chosen, w1 and w2 are given immediately in terms of xi:
+x1_new = sol[0]
 w1_new = (1. / 6) / (xi+1)
 w2_new = (xi / 6) / (xi+1)
 
 if iflag != 1:
     print(mesg)
 else:
-    print('w1_new = {}, x1_new = {},\nw2_new = {}, x2_new = {}'.format(w1_new, x1_new[0],
+    print('---')
+    print('Final residual = {}'.format(infodict['fvec']))
+    print('Number of function evaluations = {}'.format(infodict['nfev']))
+    print('w1_new = {}, x1_new = {},\nw2_new = {}, x2_new = {}'.format(w1_new, x1_new,
                                                                        w2_new, x2_new))
+    # Check lhs and rhs
+    lhs = w1_new * x1_new * (15*x1_new**2 - 9*x1_new + 1)
+    rhs = w2_new * x2_new * (15*x2_new**2 - 9*x2_new + 1)
+    print('---')
+    print('lhs = {}'.format(lhs))
+    print('rhs = {}'.format(rhs))
 
     # Check that this new "solution" satisfies the original equations...
+    print('---')
     for eqn in eqns:
         verified = eqn.subs([(w1, w1_new),
-                             (x1, x1_new[0]),
+                             (x1, x1_new),
                              (w2, w2_new),
                              (x2, x2_new)])
         print('verified = {}, should be 0.'.format(verified))
