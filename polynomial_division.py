510d8b265f167c16d9abaa79053466fe417d7119
John Peterson <jwpeterson@gmail.com>
2019-11-11 17:17:19 -0600

    Extract just the numerator.

--- polynomial_division.py
+++ polynomial_division.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
-from sympy import pdiv, simplify
+import sympy
 from sympy.abc import a, x
 from fractions import Fraction
 
 # Test from website: https://docs.sympy.org/latest/modules/polys/reference.html
 # The return value is a (result, remainder) pair.
-# result = pdiv(x**2 + 1, 2*x - 4)
+# result = sympy.pdiv(x**2 + 1, 2*x - 4)
 # print('result={}'.format(result))
 
 # a = alpha
 # s = sigma
 s = (6*a**2 - 4*a + Fraction(1,2)) / (a * (15*a**2 -9*a  + 1))
-print('s={}'.format(s))
+# print('s={}'.format(s))
 
 # Cubic polynomial
 # x = x1
 p = 15*s*x**3 - (9*s + 6)*x**2 + (s+4)*x - Fraction(1,2)
-print('p={}'.format(p))
+# print('p={}'.format(p))
 
 # Divide out the factor x-a
-result_tuple = pdiv(p, x-a)
-result = simplify(result_tuple[0])
-remainder = simplify(result_tuple[1])
-print('result={}'.format(result))
-print('remainder={}'.format(remainder))
+result_tuple = sympy.pdiv(p, x-a)
+result = sympy.simplify(result_tuple[0])
+remainder = sympy.simplify(result_tuple[1])
+# print('result={}'.format(result))
+# print('remainder={}'.format(remainder))
+
+# The denominator is the same as the denominator of sigma, which we assumed
+# to be nonzero. Therefore, we can just worry about the numerator when searching
+# for the roots.
+numerator = sympy.collect(sympy.numer(result), x)
+print('numerator={}'.format(numerator))
