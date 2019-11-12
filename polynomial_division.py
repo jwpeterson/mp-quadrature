754dd8c7bc37f7d5a74c7ac3f50857fc20ddc74e
John Peterson <jwpeterson@gmail.com>
2019-11-11 18:10:05 -0600

    Add back in some print statements.

--- polynomial_division.py
+++ polynomial_division.py
@@ -2,6 +2,7 @@
 import sympy
 from sympy.abc import a, x
 from fractions import Fraction
+import sys
 
 # a = alpha
 # s = sigma
@@ -16,10 +17,19 @@ result_tuple = sympy.pdiv(p, x-a)
 result = sympy.simplify(result_tuple[0])
 remainder = sympy.simplify(result_tuple[1])
 
+# Confirm that the remainder is zero
+if (remainder != 0):
+    print('remainder is nonzero')
+    sys.exit(1)
+
+# Print the result, collected in x
+print('result={}'.format(sympy.collect(result, x)))
+
 # The denominator is the same as the denominator of sigma, which we assumed
 # to be nonzero. Therefore, we can just worry about the numerator when searching
 # for the roots.
-numerator = sympy.collect(sympy.numer(result), x)
+numerator = sympy.numer(result)
+print('numerator={}'.format(sympy.collect(numerator, x)))
 
 # roo is a dict, and it seems that the keys are the roots and the dict
 # values are their multiplicities (maybe?) Also, note there are two
