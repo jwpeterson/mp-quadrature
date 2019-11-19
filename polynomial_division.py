af1aa8a73b5f60bb23311292187536c6c8521d28
John Peterson <jwpeterson@gmail.com>
2019-11-19 17:13:12 -0600

    Correction: it is alpha3 not alpha1 that gives the analytical form of 'min_alpha'.

--- polynomial_division.py
+++ polynomial_division.py
@@ -49,20 +49,20 @@ for r in root_dict:
     # x1_rootA = sympy.simplify(r.subs([(a, (25 + sympy.sqrt(145))/80)]))
     # print('x1(alpha=root of A)={}, ~ {}'.format(x1_rootA, x1_rootA.evalf()))
 
-# When alpha = alpha1 = (2 + cos(theta/3)) / 6, we would like to know the analytical
-# value of x1 which should match what we called min_alpha in early versions of the
+# When alpha = alpha3 = (2 + cos(theta/3)) / 6, we would like to know the analytical
+# value of x1 which should match what we called "min_alpha" in early versions of the
 # analysis. In the formula below, "t" is theta. The discriminant Delta(alpha) = 0
 # because alpha1 is one of the roots of the cubic polynomial in Delta.
-# alpha1=cos(t/3)/6 + 1/3
-alpha1 = (2 + sympy.cos(t/3)) / 6
-print('alpha1={}'.format(alpha1))
+# alpha3=cos(t/3)/6 + 1/3
+alpha3 = (2 + sympy.cos(t/3)) / 6
+print('alpha3={}'.format(alpha3))
 
-# x1_alpha1=(-cos(t/3)/12 - cos(2*t/3)/6 + 1/10)/sin(t/3)**2
-x1_alpha1 = sympy.simplify((120*alpha1**2 - 75*alpha1 + 9) / (30 * (2*alpha1 - 1) * (6*alpha1 - 1)))
-print('x1_alpha1={}'.format(x1_alpha1))
+# x1_alpha3=(-cos(t/3)/12 - cos(2*t/3)/6 + 1/10)/sin(t/3)**2
+x1_alpha3 = sympy.simplify((120*alpha3**2 - 75*alpha3 + 9) / (30 * (2*alpha3 - 1) * (6*alpha3 - 1)))
+print('x1_alpha3={}'.format(x1_alpha3))
 
-# Make sure that x1_alpha1 approx 0.10903900907287808 by substituting in
+# Make sure that x1_alpha3 approx 0.10903900907287808 by substituting in
 # theta = pi - arctan(3,4) ~ 2.498091544796508851659834154562
-# x1_alpha1_numerical=0.109039009072877
-x1_alpha1_numerical = x1_alpha1.subs([(t,2.498091544796508851659834154562)]).evalf()
-print('x1_alpha1_numerical={}'.format(x1_alpha1_numerical))
+# x1_alpha3_numerical=0.109039009072877
+x1_alpha3_numerical = x1_alpha3.subs([(t,2.498091544796508851659834154562)]).evalf()
+print('x1_alpha3_numerical={}'.format(x1_alpha3_numerical))
