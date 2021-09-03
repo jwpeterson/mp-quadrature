196996fca5f413e6a83aa1f6808217c728664c5d
John Peterson <jwpeterson@gmail.com>
2021-09-03 15:36:35 -0500

    Simplify Ro3-invariant polynomials in polar coords
    * Updates to Control which orders of polynomials are computed.
    * Reverse order of xpower, ypower
    * Expand instead of simplify the x,y polynomials
    * Do simplification for 6th-order Ro3-invariant polynomials
    
      The script now prints e.g.
      sixth = [x**6, x**5*y, x**4*y**2, x**3*y**3, x**2*y**4, x*y**5, y**6]
    
      Reynolds_sixth_polar[0] = r**6*cos(6*theta)/32 + 5*r**6/16
      Reynolds_sixth_polar[1] = r**6*sin(6*theta)/32
      Reynolds_sixth_polar[2] = -r**6*cos(6*theta)/32 + r**6/16
      Reynolds_sixth_polar[3] = -r**6*sin(6*theta)/32
      Reynolds_sixth_polar[4] = r**6*cos(6*theta)/32 + r**6/16
      Reynolds_sixth_polar[5] = r**6*sin(6*theta)/32
      Reynolds_sixth_polar[6] = -r**6*cos(6*theta)/32 + 5*r**6/16
    
      showing that the basis in polar coordinates is actually
      r^6 * {1, cos(6*theta), sin(6*theta)}
    
      Note that this also shows we cannot pick only (xpower, ypower) =
      (even, even) to generate the basis, since only two of
    
      {x^6, x^4*y^2, x^2*y^4, y^6}
    
      are "linearly independent". Likewise, only one of
    
      {x**5*y, x**3*y**3, x*y**5}
    
      is linearly independent.
    
    * Add 5th-order case and use sy.trigsimp. This actually does a pretty
      good job of simplifying For fifth-order, the script now prints:
    
      Reynolds_quintic = [5*x**5/16 - 5*x**3*y**2/8 - 15*x*y**4/16, 9*x**4*y/16 + 3*x**2*y**3/8 - 3*y**5/16, -x**5/16 + x**3*y**2/8 + 3*x*y**4/16, 3*x**4*y/16 + x**2*y**3/8 - y**5/16, -3*x**5/16 + 3*x**3*y**2/8 + 9*x*y**4/16, -15*x**4*y/16 - 5*x**2*y**3/8 + 5*y**5/16]
      Reynolds_quintic_polar[0] = 5*r**5*cos(3*theta)/16
      Reynolds_quintic_polar[1] = 3*r**5*sin(3*theta)/16
      Reynolds_quintic_polar[2] = -r**5*cos(3*theta)/16
      Reynolds_quintic_polar[3] = r**5*sin(3*theta)/16
      Reynolds_quintic_polar[4] = -3*r**5*cos(3*theta)/16
      Reynolds_quintic_polar[5] = -5*r**5*sin(3*theta)/16
    
      showing that the basis in this case is r^5 * {cos(3*theta), sin(3*theta)}.

--- Ro3_rules/Ro3_Reynolds.py
+++ Ro3_rules/Ro3_Reynolds.py
@@ -10,9 +10,9 @@ from fractions import Fraction
 def compute_reynolds(d, x_prime):
     polys = []
     Reynolds_polys = []
-    for xpower in range(d+1):
+    for ypower in range(d+1):
         # Consider polynomial: x**xpower * y**ypower
-        ypower = d - xpower
+        xpower = d - ypower
         polys.append(x**xpower * y**ypower)
         # Compute Reynolds operator for current polynomial
         Reynolds = 0
@@ -23,18 +23,71 @@ def compute_reynolds(d, x_prime):
             # of the Reynolds operator, but we consider any scalar
             # multiple of a given basis function to be equivalent.
             Reynolds += Fraction(1,3) * xi**xpower * yi**ypower
-        Reynolds_polys.append(sy.simplify(Reynolds))
+
+        # Expand so that we get "monomial" terms instead of e.g.
+        # (x + sqrt(3)*y)**6
+        Reynolds_polys.append(sy.expand(Reynolds))
 
     return polys, Reynolds_polys
 
 # Converts input vector of Reynolds polynomials defined in (x,y)
 # coordinates to polar coordinates, and returns the results.
-def to_polar(Reynolds_polys):
+def to_polar(d, Reynolds_polys):
     Reynolds_polar = []
-    r = sy.sympify('r')
+    r, theta = sy.sympify('r, theta')
     for q in Reynolds_polys:
         q_polar = q.subs([(x, r*sy.cos(theta)), (y, r*sy.sin(theta))])
-        Reynolds_polar.append(sy.simplify(q_polar))
+
+        # Debugging
+        # print(f'q_polar = {q_polar}')
+
+        Reynolds_polar.append(sy.expand(q_polar))
+
+        if d==5:
+            Reynolds_polar[-1] = sy.trigsimp(Reynolds_polar[-1])
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1])
+            # Replace cos^3(theta)
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.cos(theta)**3, Fraction(1,4)*(3*sy.cos(theta) + sy.cos(3*theta)))
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1])
+            # Replace cos(alpha) * cos(beta) = (1/2) * (cos(alpha-beta) + cos(alpha+beta))
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.cos(theta)*sy.cos(2*theta), Fraction(1,2)*(sy.cos(theta) + sy.cos(3*theta)))
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1])
+
+        if d==6:
+            # Trig. substitutions:
+            # https://en.wikipedia.org/wiki/List_of_trigonometric_identities
+            # The following are tailored to the 6th-order case; it would be good
+            # if we could generalize this.
+
+            # Replace cos^2(theta) and sin^2(theta) with multiple angle representations
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.cos(theta)**2, Fraction(1,2)*(1 + sy.cos(2*theta)))
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.sin(theta)**2, Fraction(1,2)*(1 - sy.cos(2*theta)))
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1]) # expand before substituting next
+
+            # Replace sin(theta)*cos(theta) with sin(2*theta)/2
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.cos(theta)*sy.sin(theta), Fraction(1,2)*(sy.sin(2*theta)))
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1]) # expand before substituting next
+
+            # Debugging
+            # print(f'Reynolds_polar[-1] = {Reynolds_polar[-1]}')
+
+            # Replace third powers of cos(2*theta) and sin(2*theta) with multiple angle representations
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.cos(2*theta)**3, Fraction(1,4)*(3*sy.cos(2*theta) + sy.cos(6*theta)))
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.sin(2*theta)**3, Fraction(1,4)*(3*sy.sin(2*theta) - sy.sin(6*theta)))
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1]) # expand before substituting next
+
+            # Debugging
+            # print(f'Reynolds_polar[-1] = {Reynolds_polar[-1]}')
+
+            # Replace cos^4(theta) and sin^4(theta)
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.cos(theta)**4, Fraction(1,8)*(3 + 4*sy.cos(2*theta) + sy.cos(4*theta)))
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.sin(theta)**4, Fraction(1,8)*(3 - 4*sy.cos(2*theta) + sy.cos(4*theta)))
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1]) # expand before substituting next
+
+            # Replace sin(2*theta)*cos(4*theta) with (1/2) * (sin(6*theta) + sin(-2*theta))
+            Reynolds_polar[-1] = Reynolds_polar[-1].subs(sy.sin(2*theta)*sy.cos(4*theta), Fraction(1,2)*(sy.sin(6*theta) - sy.sin(2*theta)))
+            Reynolds_polar[-1] = sy.expand(Reynolds_polar[-1]) # expand before substituting next
+
     return Reynolds_polar
 
 ####################
@@ -64,47 +117,60 @@ x_prime = [g[i] * vec_x for i in range(3)]
 
 # Compute Reynolds operator for linear basis functions.
 # Note: there are no linear Ro3-invariant polynomials!
-linear, Reynolds_linear = compute_reynolds(1, x_prime)
-print(f'linear = {linear}')
-print(f'Reynolds_linear = {Reynolds_linear}')
+if False:
+    linear, Reynolds_linear = compute_reynolds(1, x_prime)
+    print(f'linear = {linear}')
+    print(f'Reynolds_linear = {Reynolds_linear}')
+    print('')
 
-print('')
-
+# Second-order
 # Compute Reynolds operator for quadratic basis functions.
 # Note: there is only 1 unique quadratic Ro3-invariant polynomial:
 # P2 := r^2
-quadratic, Reynolds_quadratic = compute_reynolds(2, x_prime)
-Reynolds_quadratic_polar = to_polar(Reynolds_quadratic)
-print(f'quadratic = {quadratic}')
-print(f'Reynolds_quadratic = {Reynolds_quadratic}')
-print(f'Reynolds_quadratic_polar = {Reynolds_quadratic_polar}')
+if False:
+    quadratic, Reynolds_quadratic = compute_reynolds(2, x_prime)
+    Reynolds_quadratic_polar = to_polar(2, Reynolds_quadratic)
+    print(f'quadratic = {quadratic}')
+    print(f'Reynolds_quadratic = {Reynolds_quadratic}')
+    print(f'Reynolds_quadratic_polar = {Reynolds_quadratic_polar}')
+    print('')
 
-print('')
-
+# Third-order
 # Compute Reynolds operator for cubic basis functions.
 # Note: there are 2 unique quadratic Ro3-invariant polynomials:
 # P3a := r^3 * cos(theta)
 # P3b := r^3 * sin(theta)
-cubic, Reynolds_cubic = compute_reynolds(3, x_prime)
-Reynolds_cubic_polar = to_polar(Reynolds_cubic)
-print(f'cubic = {cubic}')
-print(f'Reynolds_cubic = {Reynolds_cubic}')
-print(f'Reynolds_cubic_polar = {Reynolds_cubic_polar}')
+if False:
+    cubic, Reynolds_cubic = compute_reynolds(3, x_prime)
+    Reynolds_cubic_polar = to_polar(3, Reynolds_cubic)
+    print(f'cubic = {cubic}')
+    print(f'Reynolds_cubic = {Reynolds_cubic}')
+    print(f'Reynolds_cubic_polar = {Reynolds_cubic_polar}')
+    print('')
 
-print('')
-
+# 4th-order
 # Compute Reynolds operator for quartic basis functions.
 # Note: there is 1 unique quartic Ro3-invariant polynomial,
 # P4 := P2**2 == r^4
 # This can be seen as a consequence of the fact that the only way to
 # get 4 by raising 2 and 3 to powers is by squaring 2.
-quartic, Reynolds_quartic = compute_reynolds(4, x_prime)
-Reynolds_quartic_polar = to_polar(Reynolds_quartic)
-print(f'quartic = {quartic}')
-print(f'Reynolds_quartic = {Reynolds_quartic}')
-print(f'Reynolds_quartic_polar = {Reynolds_quartic_polar}')
+if False:
+    quartic, Reynolds_quartic = compute_reynolds(4, x_prime)
+    Reynolds_quartic_polar = to_polar(4, Reynolds_quartic)
+    print(f'quartic = {quartic}')
+    print(f'Reynolds_quartic = {Reynolds_quartic}')
+    print(f'Reynolds_quartic_polar = {Reynolds_quartic_polar}')
+    print('')
 
-print('')
+# 5th-order
+if True:
+    quintic, Reynolds_quintic = compute_reynolds(5, x_prime)
+    Reynolds_quintic_polar = to_polar(5, Reynolds_quintic)
+    print(f'quintic = {quintic}')
+    print(f'Reynolds_quintic = {Reynolds_quintic}')
+    for i in range(len(Reynolds_quintic_polar)):
+        print(f'Reynolds_quintic_polar[{i}] = {Reynolds_quintic_polar[i]}')
+    print('')
 
 # At 6th-order, the basis functions are computed by taking P2^r * P3a^s * P3b^t, where:
 # r & s & t
@@ -128,13 +194,16 @@ print('')
 # r^6 * {1, cos(6*theta), sin(6*theta)}
 #
 # we can represent any 6th-order Ro3-invariant polynomial.
-sixth, Reynolds_sixth = compute_reynolds(6, x_prime)
-Reynolds_sixth_polar = to_polar(Reynolds_sixth)
-print(f'sixth = {sixth}')
-print(f'Reynolds_sixth = {Reynolds_sixth}')
-print(f'Reynolds_sixth_polar = {Reynolds_sixth_polar}')
+if False:
+    sixth, Reynolds_sixth = compute_reynolds(6, x_prime)
+    Reynolds_sixth_polar = to_polar(6, Reynolds_sixth)
+    print(f'sixth = {sixth}')
+    # print(f'Reynolds_sixth = {Reynolds_sixth}')
+    for i in range(len(Reynolds_sixth_polar)):
+        print(f'Reynolds_sixth_polar[{i}] = {Reynolds_sixth_polar[i]}')
 
-print('')
+    print('')
+
 
 # 10th-order.
 # These are formed by taking P2^r * P3a^s * P3b^t, where:
@@ -150,8 +219,9 @@ print('')
 # r^10 * {1, cos(6*theta), sin(6*theta)}
 #
 # to represent any 10th-order Ro3-invariant polynomial
-tenth, Reynolds_tenth = compute_reynolds(10, x_prime)
-Reynolds_tenth_polar = to_polar(Reynolds_tenth)
-print(f'tenth = {tenth}')
-print(f'Reynolds_tenth = {Reynolds_tenth}')
-print(f'Reynolds_tenth_polar = {Reynolds_tenth_polar}')
+if False:
+    tenth, Reynolds_tenth = compute_reynolds(10, x_prime)
+    Reynolds_tenth_polar = to_polar(10, Reynolds_tenth)
+    print(f'tenth = {tenth}')
+    print(f'Reynolds_tenth = {Reynolds_tenth}')
+    print(f'Reynolds_tenth_polar = {Reynolds_tenth_polar}')
