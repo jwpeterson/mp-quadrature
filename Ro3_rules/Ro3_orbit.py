db05ae14be224da39df05a8a0a39defd607528a2
John Peterson <jwpeterson@gmail.com>
2021-09-09 09:16:02 -0500

    Store monomials in a dict, compute in a loop
    * Delete original implementation that did not use dicts
    * Fix docs
    * Specify linear combination coeffs in dict as well
    * Add function which computes linear combinations
    * Call linear_comb() function for quintics
    * Add function to build polynomial string from coeffs
    * Call poly_string for quintic case

--- Ro3_rules/Ro3_orbit.py
+++ Ro3_rules/Ro3_orbit.py
@@ -3,6 +3,39 @@
 import sympy as sy
 from fractions import Fraction
 
+# Compute (and expand) a linear combination from the given
+# coefficients and monomials dicts.
+def linear_comb(coeffs, monomials):
+    comb = 0
+    for key, val in coeffs.items():
+        comb += val * monomials[key]
+    return sy.expand(comb)
+
+# Returns a polynomial string with the given coefficients and terms for printing
+def poly_string(coeffs):
+    poly_string = ''
+    count = 0
+    for key, val in coeffs.items():
+        # For positive coeffs after the first, print a plus sign
+        if count > 0 and val > 0:
+            poly_string += ' + '
+
+        # For negative coeffs after the first, we need a space
+        if count > 0 and val < 0 and val != -1:
+            poly_string += ' '
+
+        # For -1 coefficients print only a minus sign
+        if val == -1:
+            poly_string += ' - '
+
+        # Print "coeff*" unless coeff is 1 or -1
+        if val != 1 and val != -1:
+            poly_string += str(val) + '*'
+
+        poly_string += key
+        count += 1
+    return poly_string
+
 # Check whether the orbit:
 # {(a,b), (1-a-b,a), (b, 1-a-b)}
 # gives the same quadrature contribution for some different polynomials
@@ -10,66 +43,60 @@ from fractions import Fraction
 a, b = sy.sympify('a, b')
 x = [(a,b), (1-a-b,a), (b, 1-a-b)]
 
-x2 = 0
-y2 = 0
-x3 = 0
-x2y = 0
-xy2 = 0
-y3 = 0
-x4 = 0
-x5 = 0
-x4y = 0
-x3y2 = 0
-x2y3 = 0
-xy4 = 0
-for i in range(3):
-    # shorthand notation
-    xi = x[i][0]
-    yi = x[i][1]
-    # quadratic terms
-    x2 += xi**2
-    y2 += yi**2
-    # cubic terms - only 2 of 4 are linearly independent
-    x3 += xi**3
-    x2y += xi**2 * yi
-    xy2 += xi * yi**2
-    y3 += yi**3
-    # quartic
-    x4 += xi**4
-    # quintic - only 2 of 5 are linearly independent
-    x5 += xi**5
-    x4y += xi**4 * yi
-    x3y2 += xi**3 * yi**2
-    x2y3 += xi**2 * yi**3
-    xy4 += xi * yi**4
+# Compute monomials from degree 2 up to degree dmax. Initialize the
+# dict with the constant "1" term.
+dmax = 5
+monomials = {'1':1}
+for d in range(2, dmax+1):
+    for ypower in range(d+1):
+        for i in range(3):
+            # The xpower is the complement of the ypower
+            xpower = d - ypower
 
-# print(f'x2={sy.expand(x2)}')
-# print(f'y2={sy.expand(y2)}')
+            # Create dict key. We omit terms with 0 exponent, but we
+            # explicitly include 1 exponents.
+            key = ''
+            if xpower > 0:
+                key += 'x' + str(xpower)
+            if ypower > 0:
+                key += 'y' + str(ypower)
 
-# cubic
-# print(f'x3={sy.expand(x3)}')
-# print(f'x2y={sy.expand(x2y)}')
-# print(f'xy2={sy.expand(xy2)}')
-# print(f'y3={sy.expand(y3)}')
+            # Debugging
+            # print(f'key={key}')
 
-# We can find a non-trivial linear combination of cubic and quadratic
-# polynomials that gives us zero. This means that these 4 polynomials
-# are _not_ linearly independent!
+            # Compute monomial
+            xi = x[i][0]
+            yi = x[i][1]
+
+            # KeyError: 'x3'
+            # monomials[key] += (xi**xpower) * (yi**ypower)
+
+            # Doing this in 1 line doesn't work for some reason, I think
+            # it is the += operator? The error is:
+            # SyntaxError: can't assign to function call
+            # monomials.setdefault(key, 0) += (xi**xpower) * (yi**ypower)
+
+            # So we use two lines
+            monomial = monomials.setdefault(key, 0)
+            monomials[key] += (xi**xpower) * (yi**ypower)
+
+# Debugging
+print('monomials =')
+for key, val in monomials.items():
+    print(f'  {key} = {sy.expand(val)}')
+
+# Cubic
+# x**3 + x**2y + xy**2 - x**2 = 0
+cubic_coeffs = {'x3':1, 'x2y1':1, 'x1y2':1, 'x2':-1}
 print('')
 print('Results:')
 print('1.) Linear combination of cubic polynomials which is not LI:')
-print(f'x**3 + x**2y + xy**2 - x**2 = {sy.expand(x3 + x2y + xy2 - x2)}')
+print(f'{poly_string(cubic_coeffs)} = {linear_comb(cubic_coeffs, monomials)}')
 print('')
 
-# quartic
-# print(f'x4={sy.expand(x4)}')
-
-# quintic
-# print(f'x5={sy.expand(x5)}')
-# print(f'x4y={sy.expand(x4y)}')
-# print(f'x3y2={sy.expand(x3y2)}')
-# print(f'x2y3={sy.expand(x2y3)}')
-# print(f'xy4={sy.expand(xy4)}')
-
+# Quintic
+# 4 x**5 + 10 (x**4 y + x**3 y**2) - 5 x**4 - 10 x**2 y + 1
+quintic_coeffs = {'x5':4, 'x4y1':10, 'x3y2':10, 'x4':-5, 'x2y1':-10, '1':1}
 print('2.) Linear combination of quintic polynomials which is not LI:')
-print(f'4 x**5 + 10 (x**4 y + x**3 y**2) - 5 x**4 - 10 x**2 y + 1 = {sy.expand(4*x5 + 10*(x4y + x3y2) - 5*x4 - 10*x2y + 1)}')
+print(f'{poly_string(quintic_coeffs)} = {linear_comb(quintic_coeffs, monomials)}')
+print('')
