282c3f13e498f2f773ea4369304ac43ba2eb468b
John Peterson <jwpeterson@gmail.com>
2021-09-10 16:53:09 -0500

    Add results for 7th-order.
    * Set up linear system of equations by hand to solve for sixth-order coeffs
    * Scale to get integer coeffs
    * Update docs

--- Ro3_rules/Ro3_orbit.py
+++ Ro3_rules/Ro3_orbit.py
@@ -84,7 +84,7 @@ x = [(a,b), (1-a-b,a), (b, 1-a-b)]
 # dict with the constant "1" term. Note that Orb(1) = 3 technically but we
 # are free to "normalize" any orbit by multiplying it by a scalar, so we just
 # leave it as 1.
-dmax = 6
+dmax = 7
 monomial_orbits = {'1':1}
 for d in range(2, dmax+1):
     for ypower in range(d+1):
@@ -122,7 +122,7 @@ for key, val in monomial_orbits.items():
 print('')
 print('Results:')
 
-# Quadratic
+# Quadratic (a_2 = 1)
 # Orb(x**2) + 2*Orb(x*y) - 1 = 0
 quadratic_coeffs = {'x2':1, 'x1y1':2, '1':-1}
 print('')
@@ -130,21 +130,21 @@ print('> Linear combination of quadratic monomial orbits which is not LI:')
 print(f'{poly_string(quadratic_coeffs)} = {linear_comb(quadratic_coeffs, monomial_orbits)}')
 print('')
 
-# Cubic
+# Cubic (a_3 = 2)
 # Orb(x**3) + Orb(x**2y) + Orb(xy**2) - Orb(x**2) = 0
 cubic_coeffs = {'x3':1, 'x2y1':1, 'x1y2':1, 'x2':-1}
 print('> Linear combination of cubic monomial orbits which is not LI:')
 print(f'{poly_string(cubic_coeffs)} = {linear_comb(cubic_coeffs, monomial_orbits)}')
 print('')
 
-# Quintic
+# Quintic (a_5 = 2)
 # 4*Orb(x**5) + 10*Orb(x**4 y + x**3 y**2) - 5*Orb(x**4) - 10*Orb(x**2 y) + 1
 quintic_coeffs = {'x5':4, 'x4y1':10, 'x3y2':10, 'x4':-5, 'x2y1':-10, '1':1}
 print('> Linear combination of quintic monomial orbits which is not LI:')
 print(f'{poly_string(quintic_coeffs)} = {linear_comb(quintic_coeffs, monomial_orbits)}')
 print('')
 
-# Sixth-order
+# Sixth-order (a_6 = 3)
 # The following degree=six coeffs are the (scaled) entries of the nullspace of the matrix A_sym,
 # and we can indeed verify that the 6th-order terms are now cancelled by this linear combination.
 # The remaining terms of the linear combination were then found by "inspecting" the remaining
@@ -154,9 +154,25 @@ print('> Linear combination of sixth order monomial orbits which is not LI:')
 print(f'{poly_string(sixth_coeffs)} = {linear_comb(sixth_coeffs, monomial_orbits)}')
 print('')
 
+# Seventh-order (a_7 = 2)
+# Linear system of equations to cancel 6th-order terms
+# A = sy.Matrix([[2, -1, 1, -7], [6, -4, 4, -14], [15, -10, 7, 0]])
+# print(f'A={A}')
+# print(f'A.rref()={A.rref()}')
+# Result
+# [1, 0, 0,    -7],
+# [0, 1, 0, -56/3],
+# [0, 0, 1, -35/3]]), (0, 1, 2))
+# seventh_coeffs = {'x7':2, 'x6y1':7, 'x5y2':7}
+seventh_coeffs = {'x7':18, 'x6y1':63, 'x5y2':63, 'x6':-63, 'x5y1':-168, 'x4y2':-105, 'x5':63, 'x4y1':105, 'x3':-35, 'x2':21, '1':-4}
+print('> Linear combination of seventh order monomial orbits which is not LI:')
+print(f'{poly_string(seventh_coeffs)} = {linear_comb(seventh_coeffs, monomial_orbits)}')
+print('')
 
-# Sixth-order
-d = 6
+
+
+# Compute coefficients of linear combination that sum to zero.
+d = 7
 
 # Number of basis functions to consider for this d.
 # .) If n = a_d(d) + 1, then we should get a non-trivial nullspace
@@ -164,8 +180,8 @@ d = 6
 # .) If n = a_d(d), we should get only the trivial nullspace.
 # For example, in the d=6 case, if we consider n=4 basis functions (up
 # to x^3 * y^3) then we should get linear dependence.
-# n = a_d(d)
-n = a_d(d) + 1
+n = a_d(d)
+# n = a_d(d) + 1
 
 # Matrix to store coeffs
 # We use a sympy Matrix since that has the nullspace() command
@@ -214,9 +230,9 @@ nullsp = A_sym.nullspace()
 # from sympy import shape # does not work
 # print(f'Shape of nullspace = {nullsp.shape}') # no
 if not nullsp:
-    print(f'Basis with {n} monomials is linearly independent.')
+    print(f'd = {d} basis with {n} monomials is linearly independent.')
 else:
-    print(f'Basis with {n} monomials is linearly dependent with nullspace =\n{nullsp}')
+    print(f'd = {d} basis with {n} monomials is linearly dependent with nullspace =\n{nullsp}')
 
 # Test a_d() function
 # print(f'a_0 = {a_d(0)} (should be 1)')
