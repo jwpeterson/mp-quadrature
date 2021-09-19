112a8ee688ccb1b3a2bdd2c520c2e1db4c8356d0
John Peterson <jwpeterson@gmail.com>
2021-09-19 15:45:22 -0500

    Ro3_orbit.py: misc. updates and cleanup
    * Update script docs
    * Move code for computing nullspace to a separate function.
    * Compute nullspaces for polynomials up to degree dmax
    * Add code to compute linear combination from nullspace, and confirm that it has degree < d
    * Extract nullspace coeffs into a list and update print statement
    * Increase dmax to 8
    * Drop printing of monomial orbits
    * Move code for computing monomial orbits to separate function.
    * Update/clean up output

--- Ro3_rules/Ro3_orbit.py
+++ Ro3_rules/Ro3_orbit.py
@@ -71,61 +71,179 @@ def a_d(d):
     # The last entry in a is the one we requested
     return a[-1]
 
+# Given degree d and a the set of monomial_orbits of at least degree
+# d, computes the nullspace for the orbits of degree d. If the
+# nullspace is non-trivial, this gives us a linear combination of
+# degree d polynomials which is linearly-dependent.
+def compute_nullspace(d, monomial_orbits):
+    # The input monomial_orbits dict contains polynomials a^p * b^q
+    a, b = sy.sympify('a, b')
 
+    # Number of basis functions to consider for this d.
+    # .) If n = a_d(d) + 1, then we should get a non-trivial nullspace
+    #    for the linear system of equations that we build.
+    # .) If n = a_d(d), we should get only the trivial nullspace.
+    # For example, in the d=6 case, if we consider n=4 basis functions (up
+    # to x^3 * y^3) then we should get linear dependence.
+    # n = a_d(d)
+    n = a_d(d) + 1
 
-# Check whether the orbit:
-# {(a,b), (1-a-b,a), (b, 1-a-b)}
-# gives the same quadrature contribution for some different polynomials
+    # Matrix to store coeffs
+    # We use a sympy Matrix since that has the nullspace() command
+    A_sym = sy.zeros(n, n)
 
-a, b = sy.sympify('a, b')
-x = [(a,b), (1-a-b,a), (b, 1-a-b)]
+    for ypower in range(n):
+        xpower = d - ypower
+        key = key_from_powers(xpower, ypower)
+
+        # Debugging
+        # print(f'Coeffs of {key}:')
+
+        # Build a Poly object out of the orbit with this key. We can then
+        # call coeff_monomial() on the Poly object.
+        poly = sy.Poly(monomial_orbits[key])
+
+        # Debugging
+        # print(f'poly = {poly}')
+
+        # Extract powers of a^p * b^q in loop
+        for bpower in range(n):
+            apower = d - bpower
+            coeff = poly.coeff_monomial(a**apower * b**bpower)
+
+            # Debugging
+            # print(f'coeff of a^{apower} * b^{bpower} = {coeff}')
+
+            # We fill in the matrix of coefficients column by column.
+            # This way, the first row of A is all the a^p coeffs, the
+            # second row is all the a^{p-1} * b coeffs, etc.
+            # Store in array
+            A_sym[bpower, ypower] = coeff
+
+    # Print result
+    # print(f'A_sym = {A_sym}')
+
+    # Returns a list of sympy matrices (vectors) representing the nullspace
+    # Compute nullspace of this matrix.
+    # For d=6, with n=4, for example, the result is:
+    # [1/2],
+    # [3/2],
+    # [3/2],
+    # [  1]
+    nullsp = A_sym.nullspace()
+
+    # from sympy import shape # does not work
+    # print(f'Shape of nullspace = {nullsp.shape}') # no
+    if not nullsp:
+        print(f'd = {d} basis with {n} monomials is linearly independent.')
+    else:
+        # Compute linear combination of polynomials according to the
+        # coeffs in nullsp.  We should find that it is at most degree
+        # d-1, in other words, that the degree d terms have all
+        # cancelled. Notes:
+        # 1. We assume that the "nullsp" list has only a single entry,
+        #    and we refer directly to it in the loop below.
+        # 2. We convert to Poly when building the linear combination, since
+        #    I was getting an error for the degree 2 case when trying to
+        #    construct the Poly later.
+
+        # Extract nullspace coeffs into list. Currently "nullsp" is a
+        # list of Sympy column Matrix objects, which is not as
+        # straightforward to work with.
+        nullspace_coeffs = [val for val in nullsp[0].col(0)]
+        print(f'd = {d} basis with {n} monomials is linearly dependent with nullspace = {nullspace_coeffs}')
+
+        linear_comb = 0
+        for ypower in range(n):
+            xpower = d - ypower
+            key = key_from_powers(xpower, ypower)
+            linear_comb += nullspace_coeffs[ypower] * sy.Poly(monomial_orbits[key])
+
+        # Debugging: print result
+        # print(f'linear_comb = {linear_comb}')
+
+        # Now check the degree of linear_comb, which should be <= d,
+        # confirming that the polynomials tested are not linearly
+        # independent.
+        linear_comb_degree = sy.degree(linear_comb)
+        # print(f'linear_comb_degree = {linear_comb_degree}')
+        if not linear_comb_degree < d:
+            raise RuntimeError(f'Expected linear combination to have degree < {d}, but got {linear_comb_degree} instead')
+
 
 # Compute orbits for monomials from degree 2 up to degree dmax. Initialize the
 # dict with the constant "1" term. Note that Orb(1) = 3 technically but we
 # are free to "normalize" any orbit by multiplying it by a scalar, so we just
 # leave it as 1.
-dmax = 7
-monomial_orbits = {'1':1}
-for d in range(2, dmax+1):
-    for ypower in range(d+1):
-        for i in range(3):
-            # The xpower is the complement of the ypower
-            xpower = d - ypower
+def compute_monomial_orbits(dmax):
+    a, b = sy.sympify('a, b')
+    x = [(a,b), (1-a-b,a), (b, 1-a-b)]
+    monomial_orbits = {'1':1}
+    for d in range(2, dmax+1):
+        for ypower in range(d+1):
+            for i in range(3):
+                # The xpower is the complement of the ypower
+                xpower = d - ypower
 
-            # Create dict key.
-            key = key_from_powers(xpower, ypower)
+                # Create dict key.
+                key = key_from_powers(xpower, ypower)
 
-            # Debugging
-            # print(f'key={key}')
+                # Debugging
+                # print(f'key={key}')
 
-            # Compute monomial
-            xi = x[i][0]
-            yi = x[i][1]
+                # Compute monomial
+                xi = x[i][0]
+                yi = x[i][1]
 
-            # KeyError: 'x3'
-            # monomial_orbits[key] += (xi**xpower) * (yi**ypower)
+                # KeyError: 'x3'
+                # monomial_orbits[key] += (xi**xpower) * (yi**ypower)
 
-            # Doing this in 1 line doesn't work for some reason, I think
-            # it is the += operator? The error is:
-            # SyntaxError: can't assign to function call
-            # monomial_orbits.setdefault(key, 0) += (xi**xpower) * (yi**ypower)
+                # Doing this in 1 line doesn't work for some reason, I think
+                # it is the += operator? The error is:
+                # SyntaxError: can't assign to function call
+                # monomial_orbits.setdefault(key, 0) += (xi**xpower) * (yi**ypower)
 
-            # So we use two lines
-            monomial = monomial_orbits.setdefault(key, 0)
-            monomial_orbits[key] += (xi**xpower) * (yi**ypower)
+                # So we use two lines
+                monomial = monomial_orbits.setdefault(key, 0)
+                monomial_orbits[key] += (xi**xpower) * (yi**ypower)
 
-# Debugging
-print('monomial_orbits =')
-for key, val in monomial_orbits.items():
-    print(f'  {key} = {sy.expand(val)}')
+    # Debugging
+    # print('monomial_orbits =')
+    # for key, val in monomial_orbits.items():
+    #     print(f'  {key} = {sy.expand(val)}')
+    return monomial_orbits
 
-print('')
-print('Results:')
+
+
+"""
+This code can be used to find linear combinations of monomial orbits
+which are linearly dependent. For a given monomial
+x^m * y^n, with m+n=d, we compute
+
+Orb(x^m * y^n) := sum_{q=1}^3 (x_q)^m * (y_q)^n
+
+where:
+
+(x_q, y_q) \in {(a,b), (1-a-b,a), (b, 1-a-b)}
+
+is the set of Ro3 points generated from the arbitrary input (a,b).
+This gives a polynomial of total degree d in a and b. We can then find
+a linear combination of a_d + 1 such polynomials which is linearly
+dependent, and this confirms that we need only consider a_d polynomials
+for each d.
+"""
+
+# Compute orbits for monomials from degree 2 up to degree dmax.
+dmax = 8
+monomial_orbits = compute_monomial_orbits(dmax)
+
+print('--------------------------------------------------------------------------------')
+print('I. Linear combinations of Ro3-invariant polynomials that sum to zero (are not LI)')
+print('--------------------------------------------------------------------------------')
 
 # Quadratic (a_2 = 1)
 # Orb(x**2) + 2*Orb(x*y) - 1 = 0
 quadratic_coeffs = {'x2':1, 'x1y1':2, '1':-1}
-print('')
 print('> Linear combination of quadratic monomial orbits which is not LI:')
 print(f'{poly_string(quadratic_coeffs)} = {linear_comb(quadratic_coeffs, monomial_orbits)}')
 print('')
@@ -163,76 +281,19 @@ print('')
 # [1, 0, 0,    -7],
 # [0, 1, 0, -56/3],
 # [0, 0, 1, -35/3]]), (0, 1, 2))
-# seventh_coeffs = {'x7':2, 'x6y1':7, 'x5y2':7}
 seventh_coeffs = {'x7':18, 'x6y1':63, 'x5y2':63, 'x6':-63, 'x5y1':-168, 'x4y2':-105, 'x5':63, 'x4y1':105, 'x3':-35, 'x2':21, '1':-4}
 print('> Linear combination of seventh order monomial orbits which is not LI:')
 print(f'{poly_string(seventh_coeffs)} = {linear_comb(seventh_coeffs, monomial_orbits)}')
 print('')
 
+# Compute nullspaces for degrees up to dmax
+# for d in range(2,4):
+print('--------------------------------------------------------------------------------')
+print(f'II. Nullspaces for degrees up to {dmax}')
+print('--------------------------------------------------------------------------------')
+for d in range(2,dmax+1):
+    compute_nullspace(d, monomial_orbits)
 
-
-# Compute coefficients of linear combination that sum to zero.
-d = 7
-
-# Number of basis functions to consider for this d.
-# .) If n = a_d(d) + 1, then we should get a non-trivial nullspace
-#    for the linear system of equations that we build.
-# .) If n = a_d(d), we should get only the trivial nullspace.
-# For example, in the d=6 case, if we consider n=4 basis functions (up
-# to x^3 * y^3) then we should get linear dependence.
-n = a_d(d)
-# n = a_d(d) + 1
-
-# Matrix to store coeffs
-# We use a sympy Matrix since that has the nullspace() command
-A_sym = sy.zeros(n, n)
-
-for ypower in range(n):
-    xpower = d - ypower
-    key = key_from_powers(xpower, ypower)
-
-    # Debugging
-    # print(f'Coeffs of {key}:')
-
-    # Build a Poly object out of the orbit with this key. We can then
-    # call coeff_monomial() on the Poly object.
-    poly = sy.Poly(monomial_orbits[key])
-
-    # Debugging
-    # print(f'poly = {poly}')
-
-    # Extract powers of a^p * b^q in loop
-    for bpower in range(n):
-        apower = d - bpower
-        coeff = poly.coeff_monomial(a**apower * b**bpower)
-
-        # Debugging
-        # print(f'coeff of a^{apower} * b^{bpower} = {coeff}')
-
-        # We fill in the matrix of coefficients column by column.
-        # This way, the first row of A is all the a^p coeffs, the
-        # second row is all the a^{p-1} * b coeffs, etc.
-        # Store in array
-        A_sym[bpower, ypower] = coeff
-
-# Print result
-# print(f'A_sym = {A_sym}')
-
-# Returns a list of sympy matrices (vectors) representing the nullspace
-# Compute nullspace of this matrix.
-# For d=6, with n=4, for example, the result is:
-# [1/2],
-# [3/2],
-# [3/2],
-# [  1]
-nullsp = A_sym.nullspace()
-
-# from sympy import shape # does not work
-# print(f'Shape of nullspace = {nullsp.shape}') # no
-if not nullsp:
-    print(f'd = {d} basis with {n} monomials is linearly independent.')
-else:
-    print(f'd = {d} basis with {n} monomials is linearly dependent with nullspace =\n{nullsp}')
 
 # Test a_d() function
 # print(f'a_0 = {a_d(0)} (should be 1)')
